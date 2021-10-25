9961cd0c83b0166b4f582e680d5957a1bce78cac
Marcus <marcuslucas.jb@gmail.com>
2021-10-25 15:54:14 -0300

    [LinkPreview] Remove site link if is not available

--- frontend/components/URLPreview.tsx
+++ frontend/components/URLPreview.tsx
@@ -34,14 +34,16 @@ const URLPreview: React.FC<{ url: string }> = ({ url }) => {
   return (
     <div className="justify-between mt-4 border-l-2 p-2 border-blue-400 bg-blue-100 ">
       <div className="flex flex-col">
-        <a
-          href={url}
-          target="_blank"
-          rel="noreferrer"
-          className="text-blue-500 font-bold hover:underline mb-1"
-        >
-          {siteName}
-        </a>
+        {siteName && (
+          <a
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 font-bold hover:underline mb-1"
+          >
+            {siteName}
+          </a>
+        )}
         <p className="text-gray-800 font-bold mb-1">{title}</p>
         <p className="text-gray-600 text-xs mb-2">{description}</p>
         <div
