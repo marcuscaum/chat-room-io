f19c1e25d9dd1fcb35c3b0487a39ce7666daf4b5
Marcus <marcuslucas.jb@gmail.com>
2021-10-25 14:27:48 -0300

    fix

--- frontend/utils/mentionHandlers.tsx
+++ frontend/utils/mentionHandlers.tsx
@@ -1,11 +1,12 @@
 import { REGEX_VALID_EMAIL } from "../constants";
 
 export const formatMentions = (content: []): any => {
-  return content.map((part: any) =>
-    part === typeof String && part.match(REGEX_VALID_EMAIL) ? (
+  return content.map((part: any) => {
+    console.log(part);
+    return typeof part === "string" && part.match(REGEX_VALID_EMAIL) ? (
       <span className="text-blue-400 break-all font-semibold">{part}</span>
     ) : (
       part
-    )
-  );
+    );
+  });
 };
